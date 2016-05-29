431f77cca039ae500b35a1f91baac57916a07e62
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-05-29 18:52:52 +0300

    Hcard to hcard

--- lib/monopoly/hcard.rb
+++ lib/monopoly/hcard.rb
@@ -1,7 +1,7 @@
 class Hcard < Card
 
   def draw 
-    template = File.read("html/_Hcard.html.erb")
+    template = File.read("html/_hcard.html.erb")
     ERB.new(template).result(self.get_binding)
   end
 
