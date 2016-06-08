94999221a7de0cd4eb76f7ea97c06503579b6220
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-06-08 21:35:11 +0300

    backing up

--- lib/monopoly/game.rb
+++ lib/monopoly/game.rb
@@ -2,28 +2,27 @@ require 'sinatra/base'
 
 class MonopolyGame < Sinatra::Base
 
-
+  enable :sessions
 
   set :views, "#{settings.root}/../../views"
   set :public_folder , "#{settings.root}/../../assets"
 
-  enable :sessions
 
   get '/' do
     board = Board.new
-    sesion[:board] = board
+    session[:board] = board
     @top_cards, @side_cards, @bottom_cards = board.get_cards
    erb :index
   end
 
   post '/roll' do
     
-    board = sesion[:board]
+    board = session[:board]
     @top_cards, @side_cards, @bottom_cards = board.get_cards
-    new_position = boards.players.first.position + Dice.roll
+    new_position = board.players.first.position + Dice.roll
     new_position = 40 if new_position > 40
-    board.set_position(player: boards.players.first, position: new_position)
-    sesion[:board] = board
+    board.set_position(player: board.players.first, position: new_position)
+    session[:board] = board
     erb :index
   end
  
